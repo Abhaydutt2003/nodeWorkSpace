30b370f192eb59759d9a5c3712e5ff4cb18bfbb1
Abhay Dutt <dutt.abhay@gmail.com>
2023-07-28 16:02:06 +0530

    learn module

---
+++ modules/moduleDemo.js
@@ -0,0 +1,14 @@
+// import person from './operations.js';
+
+
+
+//  const p = new person("Abhay",18);
+
+
+// p.name();
+// p.age();
+
+const operations = require('./operations');
+
+console.log(operations.add(2,3));
+
---
+++ modules/operations.js
@@ -0,0 +1,4 @@
+module.exports.add  = function (a,b){
+    return a*b;
+}
+//here i am exporting the function add
