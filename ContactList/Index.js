f313c5e2842c101d32953b5a6251f36c8069e2c7
Abhay Dutt <dutt.abhay@gmail.com>
2023-08-06 22:26:48 +0530

    learn ejs

--- ContactList/Index.js
+++ ContactList/Index.js
@@ -1,18 +1,49 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/',(request,response)=>{
-    console.log(request);
-    response.send("<h1>The server is running, or is it?<h1>");
+var contactList = [
+    {
+        name: "Abhay",
+        number: "234234234"
+    },
+
+    {
+        name: "Ankita",
+        number: "984576987345"
+    },
+
+    {
+        name: "random",
+        number: "934856789435"
+    }
+];
+
+app.get('/', (request, response) => {
+    return response.render('home', {
+        title: "Contact List",
+        contact_list:contactList
+    });
 });
 
-app.listen(port, function(error){
-    if(error){
+
+app.get('/practice', (request, response) => {
+    return response.render('practice', {
+        title: "play ejs"
+    })
+});
+
+
+
+app.listen(port, function (error) {
+    if (error) {
         console.log(error);
         return;
     }
-    console.log("The server is running on port :",port);
+    console.log("The server is running on port :", port);
 });
